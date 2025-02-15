a2d4e370e07af7ab544b92e4aed2ff1e082d0611
seongjuun <beajon0301@naver.com>
2025-02-15 20:44:15 +0900

    repository(1)

--- skuhub/src/main/java/org/skuhub/skuhub/repository/taxi/TaxiJoinRepository.java
+++ skuhub/src/main/java/org/skuhub/skuhub/repository/taxi/TaxiJoinRepository.java
@@ -1,9 +1,9 @@
 package org.skuhub.skuhub.repository.taxi;
 
-import org.skuhub.skuhub.model.taxi.TaxiCommentJpaEntity;
+import org.skuhub.skuhub.model.taxi.TaxiJoinJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaxiCommentRepository extends JpaRepository<TaxiCommentJpaEntity, Integer> {
+public interface TaxiJoinRepository extends JpaRepository<TaxiJoinJpaEntity, Integer> {
 }
--- skuhub/src/main/java/org/skuhub/skuhub/repository/taxi/TaxiShareRepository.java
+++ skuhub/src/main/java/org/skuhub/skuhub/repository/taxi/TaxiShareRepository.java
@@ -1,4 +1,9 @@
 package org.skuhub.skuhub.repository.taxi;
 
-public interface TaxiShareRepository {
+import org.skuhub.skuhub.model.taxi.TaxiShareJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaxiShareRepository extends JpaRepository<TaxiShareJpaEntity, Integer> {
 }
