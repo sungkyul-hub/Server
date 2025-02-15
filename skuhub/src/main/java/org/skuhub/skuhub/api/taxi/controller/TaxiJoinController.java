b0dacc43e24ed4d08a53007ec1bc72a3fcf5c845
seongjuun <beajon0301@naver.com>
2025-02-15 20:46:05 +0900

    controller join

---
+++ skuhub/src/main/java/org/skuhub/skuhub/api/taxi/controller/TaxiJoinController.java
@@ -0,0 +1,22 @@
+package org.skuhub.skuhub.api.taxi.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.skuhub.skuhub.api.Sample.dto.request.SamplePostRequest;
+import org.skuhub.skuhub.api.Sample.dto.response.SampleResponse;
+import org.skuhub.skuhub.model.sample.SampleJpaEntity;
+import org.skuhub.skuhub.repository.sample.SampleRepository;
+import org.skuhub.skuhub.common.response.BaseResponse;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.OffsetDateTime;
+import java.util.List;  // List를 임포트
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(name = "/taxi")
+@Slf4j
+public class TaxiJoinController {
+}
