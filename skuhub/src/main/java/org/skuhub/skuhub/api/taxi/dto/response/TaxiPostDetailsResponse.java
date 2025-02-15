3c0931379cc7cb5f388705e31bb9e46ddb511f37
seongjuun <beajon0301@naver.com>
2025-02-15 15:55:06 +0900

    dto(3) 게시글 세부 내용

---
+++ skuhub/src/main/java/org/skuhub/skuhub/api/taxi/dto/response/TaxiPostDetailsResponse.java
@@ -0,0 +1,23 @@
+package org.skuhub.skuhub.api.taxi.dto.response;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.skuhub.skuhub.api.taxi.dto.request.TaxiCommentRequest;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TaxiPostDetailsResponse {
+    private int id;
+    private String title;
+    private String description;
+    private OffsetDateTime ride_time;
+    private OffsetDateTime created_at;
+    private String name;
+    private int status;
+    private int number_of_people;
+    private List<TaxiCommentRequest> comments;
+}
