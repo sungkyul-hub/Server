853a8c76e2afcf49796dbcad7809bde68d96cab0
seongjuun <beajon0301@naver.com>
2025-02-16 18:48:14 +0900

    게시글 올리기 (4)

--- skuhub/src/main/java/org/skuhub/skuhub/api/taxi/controller/TaxiPostController.java
+++ skuhub/src/main/java/org/skuhub/skuhub/api/taxi/controller/TaxiPostController.java
@@ -1,18 +1,13 @@
 package org.skuhub.skuhub.api.taxi.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.skuhub.skuhub.api.taxi.dto.request.TaxiPostRequest;
-import org.skuhub.skuhub.model.taxi.TaxiShareJpaEntity;
-import org.skuhub.skuhub.model.user.UserInfoJpaEntity;
 import org.skuhub.skuhub.repository.taxi.TaxiShareRepository;
 import org.skuhub.skuhub.repository.users.UserInfoRepository;
 import org.springframework.web.bind.annotation.*;
 import org.skuhub.skuhub.common.response.BaseResponse;
-
-import java.time.OffsetDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,27 +24,7 @@ public class TaxiPostController {
         log.info("Received post request with userKey: {}", request.getUserId());
         log.info("Received post request with Title: {}", request.getTitle());
 
-        UserInfoJpaEntity userEntity = userInfoRepository.findByUserId(request.getUserId())
-                .orElseThrow(() -> new EntityNotFoundException("User not found"));
-
-        log.info("Request received with userKey: {}", request.getUserId());
-
-        TaxiShareJpaEntity entity = new TaxiShareJpaEntity();
-        entity.setUserKey(userEntity); // userKey 설정
-        entity.setTitle(request.getTitle());
-        entity.setDepartureLocation(request.getDepartureLocation());
-        entity.setNumberOfPeople(request.getNumberOfPeople());
-        entity.setRideTime(request.getRideTime());
-        entity.setDescription(request.getDescription());
-
-        try {
-            TaxiShareJpaEntity saved = taxiShareRepository.save(entity);
-            log.info("TaxiShare saved with userKey: {}", saved.getUserKey());
-            return new BaseResponse<>(true, "200", "택시합승 게시글 저장 성공", OffsetDateTime.now(), "게시글 작성 성공");
-        } catch (Exception e) {
-            log.error("Error saving TaxiShare: {}", e.getMessage());
-            return new BaseResponse<>(false, "500", "게시글 저장 실패", OffsetDateTime.now(), e.getMessage());
-        }
+        return (BaseResponse<String>) TaxiPostService.postTaxiShare(request, taxiShareRepository, userInfoRepository);
 
     }
 }
--- skuhub/src/main/java/org/skuhub/skuhub/api/taxi/controller/TaxiPostService.java
+++ skuhub/src/main/java/org/skuhub/skuhub/api/taxi/controller/TaxiPostService.java
@@ -1,4 +1,45 @@
 package org.skuhub.skuhub.api.taxi.controller;
 
+import io.jsonwebtoken.Jwt;
+import jakarta.persistence.EntityNotFoundException;
+import org.skuhub.skuhub.api.auth.service.AuthService;
+import org.skuhub.skuhub.api.taxi.dto.request.TaxiPostRequest;
+import org.skuhub.skuhub.common.response.BaseResponse;
+import org.skuhub.skuhub.common.utills.jwt.dto.JwtDto;
+import org.skuhub.skuhub.model.taxi.TaxiShareJpaEntity;
+import org.skuhub.skuhub.model.user.UserInfoJpaEntity;
+import org.skuhub.skuhub.repository.taxi.TaxiShareRepository;
+import org.skuhub.skuhub.repository.users.UserInfoRepository;
+
+import java.time.OffsetDateTime;
+
 public class TaxiPostService {
+    private final TaxiShareRepository taxiShareRepository;
+    private final UserInfoRepository userInfoRepository;
+
+    public TaxiPostService(TaxiShareRepository taxiShareRepository, UserInfoRepository userInfoRepository, AuthService authService) {
+        this.taxiShareRepository = taxiShareRepository;
+        this.userInfoRepository = userInfoRepository;
+    }
+
+
+    public static BaseResponse<String> postTaxiShare(TaxiPostRequest request, TaxiShareRepository taxiShareRepository, UserInfoRepository userInfoRepository) {
+        UserInfoJpaEntity userEntity = userInfoRepository.findByUserId(request.getUserId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        TaxiShareJpaEntity entity = new TaxiShareJpaEntity();
+        entity.setUserKey(userEntity);
+        entity.setTitle(request.getTitle());
+        entity.setDepartureLocation(request.getDepartureLocation());
+        entity.setNumberOfPeople(request.getNumberOfPeople());
+        entity.setRideTime(request.getRideTime());
+        entity.setDescription(request.getDescription());
+
+        try {
+            TaxiShareJpaEntity saved = taxiShareRepository.save(entity);
+            return new BaseResponse<>(true, "200", "택시합승 게시글 저장 성공", OffsetDateTime.now(), "게시글 작성 성공");
+        } catch (Exception e) {
+            return new BaseResponse<>(false, "500", "게시글 저장 실패", OffsetDateTime.now(), e.getMessage());
+        }
+    }
 }
